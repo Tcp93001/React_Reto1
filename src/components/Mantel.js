34527e2cc423764dae6db262ae08dc641337dd8e
Tcp93001 <joseguerreroroman@yahoo.com.mx>
2021-11-08 22:10:29 -0600

    Cambios segunda clase

--- src/components/Mantel.js
+++ src/components/Mantel.js
@@ -2,14 +2,36 @@ import React from 'react';
 import Plato from './Plato';
 import Cubierto from './Cubierto';
 
-const Mantel = () => {
-	return (
-		<div className="mantel spaceAround">
-			<Cubierto />
-			<Plato />
-			<Cubierto />
-		</div>
-	);
+class Mantel extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			backgroundColor: 'darkSalmon',
+			height: '120px'
+		}
+	}
+
+	render() {
+		const colorValido =
+			this.props.backgroundColor !== undefined && this.props.backgroundColor !== "red" ?
+			this.props.backgroundColor : this.state.backgroundColor;
+
+		return (
+			<div
+				className="mantel spaceAround"
+				style={{
+					backgroundColor: colorValido,
+					height: this.props.height || this.state.height
+				}}
+			>
+				<Cubierto />
+				<Plato />
+				<Cubierto />
+			</div>
+		);
+	}
 };
+
 
 export default Mantel;
